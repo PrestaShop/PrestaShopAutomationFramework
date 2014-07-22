d4239643c91f3a8fd51b5932bd571022c14e6daf
fram <fm.de.jouvencel@gmail.com>
2014-07-22 11:13:57 +0200

    // renamed file

---
+++ src/Helper/Spinner.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace PrestaShop\Helper;
+
+class Spinner
+{
+	private $timeout_in_seconds;
+	private $interval_in_milliseconds;
+	private $error_message;
+
+	public function __construct(
+		$error_message = 'Could not SpinAssert something.',
+		$timeout_in_seconds = 5,
+		$interval_in_milliseconds = 500
+	)
+	{
+		$this->timeout_in_seconds = $timeout_in_seconds;
+		$this->interval_in_milliseconds = $interval_in_milliseconds;
+
+		$this->error_message = $error_message;
+	}
+
+	public function assertBecomesTrue(callable $truthy_returner)
+	{
+		$elapsed = 0;
+		$ok = false;
+		$exception = null;
+
+		do {
+			try {
+				$ok = call_user_func($truthy_returner);
+			} catch (\Exception $e) {
+				$ok = false;
+				$exception = $e;
+			}
+
+
+			usleep($this->interval_in_milliseconds * 1000);
+			$elapsed += $this->interval_in_milliseconds / 1000;
+		} while (!$ok && $elapsed < $this->timeout_in_seconds);
+
+		if (!$ok)
+		{
+			if ($this->error_message)
+			{
+				throw new \PrestaShop\Exception\SpinAssertException($this->error_message);
+			}
+			elseif ($exception)
+			{
+				throw $exception;
+			}
+			else
+			{
+				throw new \PrestaShop\Exception\SpinAssertException('Callback never returned true, but did not produce an exception either.');
+			}
+		}
+	}
+}
