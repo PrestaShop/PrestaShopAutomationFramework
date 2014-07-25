ee39e68bab458a82d85ffaeb4e9188ea1d1536a4
fram <fm.de.jouvencel@gmail.com>
2014-07-25 16:35:06 +0200

    // documentation

--- tests-available/TaxManagementTest.php
+++ tests-available/TaxManagementTest.php
@@ -5,10 +5,23 @@ namespace PrestaShop\FunctionalTest;
 /**
  * This test does the following:
  *
+ * - Check it can access AdminTaxes
+ * - Check taxes can be disabled / enabled
+ * - Check displaying taxes in the shopping cart can be enabled / disabled
+ * - Check ecotax can be enabled / disabled
+ * - Check ecotax can be set to use a specific tax group
+ * - Check that the tax can be set to be based on delivery / invoice address
+ * 
  * - Create and enable 2 tax rules, OldFrenchVat and NewFrenchVat
- * - Create a Tax Rule Group that applies OldFrenchVat to Country the country with id 1
- * - Create a Tax Rule Group that applies OldFrenchVat to all countries
- * - Create a Tax Rule Group that applies both OldFrenchVat and NewFrenchVat, combined, to all countries
+ *
+ * - Create a tax rules group with one tax, one country with zipcode range
+ * - Create a tax rules group with one tax, applied to 2 USA states
+ * - Create a tax rules group with one tax, applied to 1 USA state
+ * - Create a tax rules group with one tax, applied to 2 countries
+ * - Create a tax rules group with one tax, applied to 1 country
+ * - Create a tax rules group with two taxes, applied combined to 1 country
+ * - Create a tax rules group with one tax, applied to all countries
+ * - Create a tax rules group with two taxes, applied to all countries in combine mode
  * 
  */
 
