2f3af2915cb8792e73fd88c87c7bed60f696daf2
fram <fm.de.jouvencel@gmail.com>
2014-09-17 13:40:41 +0200

    // refacto

--- src/Helper/Spinner.php
+++ src/Helper/Spinner.php
@@ -9,7 +9,7 @@ class Spinner
 	private $error_message;
 
 	public function __construct(
-		$error_message = 'Could not SpinAssert something.',
+		$error_message = 'Could not assert something.',
 		$timeout_in_seconds = 5,
 		$interval_in_milliseconds = 500
 	)
@@ -23,39 +23,20 @@ class Spinner
 	public function assertBecomesTrue(callable $truthy_returner)
 	{
 		$elapsed = 0;
-		$ok = false;
-		$exception = null;
-
-		do {
+		while ($elapsed < $this->timeout_in_seconds)
+		{
 			try {
-				$ok = call_user_func($truthy_returner);
+				if (call_user_func($truthy_returner))
+					return true;
 			} catch (\Exception $e) {
-				$ok = false;
-				$exception = $e;
+				if ($elapsed >= $this->timeout_in_seconds)
+					throw $e;
 			}
-
-			if ($ok)
-				return;
-
 			usleep($this->interval_in_milliseconds * 1000);
 			$elapsed += $this->interval_in_milliseconds / 1000;
-		} while (!$ok && $elapsed < $this->timeout_in_seconds);
-
-		if (!$ok)
-		{
-			if ($this->error_message)
-			{
-				throw new \PrestaShop\Exception\SpinAssertException($this->error_message);
-			}
-			elseif ($exception)
-			{
-				throw $exception;
-			}
-			else
-			{
-				throw new \PrestaShop\Exception\SpinAssertException('Callback never returned true, but did not produce an exception either.');
-			}
 		}
+
+		throw new \PrestaShop\Exception\SpinAssertException($this->error_message);
 	}
 
 	public function assertNoException(callable $cb)
