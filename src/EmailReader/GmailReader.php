cc0428d45b57ba167d5e01e62664840894cc13d8
fram <fm.de.jouvencel@gmail.com>
2014-12-01 09:01:21 +0100

    Improved GmailReader to find HTML in almost all cases

--- src/EmailReader/GmailReader.php
+++ src/EmailReader/GmailReader.php
@@ -15,7 +15,31 @@ class GmailReader
 		$this->password = $password;
 	}
 
-	public function readEmails($sentTo = null)
+	private function findPart($structure, $matcher)
+	{
+		if (!isset($structure->parts)) {
+			return false;
+		}
+
+		// Search breadth first
+		foreach ($structure->parts as $n => $subStructure) {
+			if ($matcher($subStructure)) {
+				return (string)($n + 1);
+			}
+		}
+		
+		// Not found? Go deeper
+		foreach ($structure->parts as $n => $subStructure) {
+			$subPart = $this->findPart($subStructure, $matcher);
+			if (false !== $subPart) {
+				return ($n + 1) . '.' . $subPart;
+			}
+		}
+
+		return false;
+	}
+
+	public function readEmails($sentTo = null, $bodyPart = null)
 	{
 		$host = '{imap.gmail.com:993/imap/ssl}INBOX';
 
@@ -31,13 +55,25 @@ class GmailReader
 			$messages = [];
 			foreach ($emails as $n)
 			{
+				$structure = imap_fetchstructure($inbox, $n);
+
+				if (!$bodyPart) {
+					$part = $this->findPart($structure, function ($part) {
+						return $part->subtype === 'HTML';
+					});
+				} elseif (is_callable($bodyPart)) {
+					$part = $this->findPart($structure, $bodyPart);
+				} else {
+					$part = $bodyPart;
+				}
+
 				$hinfo = imap_headerinfo($inbox, $n);
 
 				$subject = $hinfo->subject;
 
 				$message = [
 					'subject' => $subject,
-					'body' => imap_fetchbody($inbox, $n, 2)
+					'body' => imap_fetchbody($inbox, $n, $part),
 				];
 
 				$messages[] = $message;
@@ -48,12 +84,25 @@ class GmailReader
 		}
 	}
 
-	public function ensureAnEmailIsSentTo($address, $timeout_in_seconds = 300)
+	public function ensureAnEmailIsSentTo($address, $timeout_in_seconds = 300, array $options = array())
 	{
-		$spinner = new Spinner("Did not get an email on $address after {$timeout_in_seconds}s.", $timeout_in_seconds, 10000);
+		$spinner = new Spinner("Did not get required email on $address after {$timeout_in_seconds}s.", $timeout_in_seconds, 10000);
 
-		$spinner->assertBecomesTrue(function () use ($address) {
-			return count($this->readEmails($address)) > 0;
+		$spinner->assertBecomesTrue(function () use ($address, $options) {
+
+			$emails = $this->readEmails($address);
+
+			if (isset($options['body']['contains'])) {
+				foreach ($emails as $email) {
+					if (false !== strpos($email['body'], $options['body']['contains'])) {
+						return true;
+					}
+				}
+				return false;
+			} else {
+				return count($emails) > 0;
+			}
+
 		});
 
 		return $this;
