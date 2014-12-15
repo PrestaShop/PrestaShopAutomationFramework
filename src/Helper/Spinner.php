aabdb4ca72425a893b1779e026ed0eebd083dd02
fram <fm.de.jouvencel@gmail.com>
2014-12-15 13:54:20 +0100

    // allow certain exceptions to skip the retry

--- src/Helper/Spinner.php
+++ src/Helper/Spinner.php
@@ -8,6 +8,8 @@ class Spinner
     private $interval_in_milliseconds;
     private $error_message;
 
+    private $passthrough = [];
+
     public function __construct(
         $error_message = 'Repeated assertion test failed after maximum waiting time was reached.',
         $timeout_in_seconds = 5,
@@ -32,7 +34,7 @@ class Spinner
                 if (!$this->error_message) {
                     $this->error_message = $e->getMessage();
                 }
-                if (!$allowExceptions) {
+                if (!$allowExceptions || $this->passesThrough($e)) {
                     break;
                 }
             }
@@ -49,11 +51,29 @@ class Spinner
             try {
                 return call_user_func($cb);
             } catch (\Exception $e) {
-                if (time() > $maxTime) {
+                if (time() > $maxTime || $this->passesThrough($e)) {
                     throw $e;
                 }
             }
             usleep($this->interval_in_milliseconds * 1000);
         }
+    }
+
+    public function addPassthroughExceptionClass($class)
+    {
+        $this->passthrough[] = $class;
+
+        return $this;
+    }
+
+    public function passesThrough(\Exception $e)
+    {
+        foreach ($this->passthrough as $class) {
+            if ($e instanceof $class) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
