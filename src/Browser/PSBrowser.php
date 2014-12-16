aa8bafe0b93951b2fee47e63356aeb182a04ffc8
fram <fm.de.jouvencel@gmail.com>
2014-12-16 11:35:56 +0100

    // some PS specific helpers

--- src/Browser/PSBrowser.php
+++ src/Browser/PSBrowser.php
@@ -2,16 +2,25 @@
 
 namespace PrestaShop\PSTAF\Browser;
 
+use PrestaShop\PSTAF\Exception\StandardSuccessMessageNotDisplayedException;
+use PrestaShop\PSTAF\Exception\StandardErrorMessageDisplayedException;
+
 class PSBrowser extends Browser
 {
 	public function ensureStandardSuccessMessageDisplayed($error_explanation = null)
 	{
-	    
+		if (!$this->hasVisible('div.alert.alert-success')) {
+			throw new StandardSuccessMessageNotDisplayedException($error_explanation); 
+		}
+
+	    return $this;
 	}
 
 	public function ensureStandardErrorMessageNotDisplayed($error_explanation = null)
 	{
-	    
+	    if ($this->hasVisible('div.alert.alert-error')) {
+	    	throw new StandardErrorMessageDisplayedException($error_explanation); 
+	    }
 	}
 
     /**
@@ -19,11 +28,11 @@ class PSBrowser extends Browser
 	*/
     public function prestaShopSwitch($idWithoutHash, $yesno)
     {
-    	
+    	return $this->clickLabelFor($idWithoutHash . ($yesno ? '_on' : '_off'));
     }
 
     public function prestaShopSwitchValue($idWithoutHash)
     {
-    	
+    	return $this->find('#'.$idWithoutHash.'_on')->isSelected();
     }
 }
