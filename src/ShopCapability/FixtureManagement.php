1a7df9d37d0a9324af0a17a5ee7647d1ad512c87
fram <fm.de.jouvencel@gmail.com>
2015-01-08 08:57:01 +0100

    // Unless otherwise specified, first thing we do is to delete the onboarding module

--- src/ShopCapability/FixtureManagement.php
+++ src/ShopCapability/FixtureManagement.php
@@ -6,8 +6,15 @@ class FixtureManagement extends ShopCapability
 {
     public function setupInitialState(array $initial_state)
     {
-        if (isset($initial_state['ShopInstallation']))
+        if (isset($initial_state['ShopInstallation'])) {
             $this->getShop()->getInstaller()->install($initial_state['ShopInstallation']);
+
+            if (empty($initial_state['ShopInstallation']['keepOnboardingModule'])) {
+                $this->getShop()->getBackOfficeNavigator()->login()->deleteModule('onboarding');
+                $this->getShop()->getBrowser()->clearCookies();
+            }
+
+        }
 
         return $this;
     }
